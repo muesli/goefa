a793ca28965eda3e0f2810e7a3600715d28ccc82
Michael Wendland <accounts@michiwend.com>
2014-08-10 00:42:10 +0200

    rm deprecated EFAProvider map

--- providers.go
+++ providers.go
@@ -47,25 +47,3 @@ func ProviderFromJson(short_name string) (*EFAProvider, error) {
 
 	return provider, nil
 }
-
-// Map of supported EFA providers
-var Providers = map[string]EFAProvider{
-
-	"avv": EFAProvider{
-		Name:                     "Augsburger Verkehrs- und Tarifverbund GmbH",
-		BaseURL:                  "http://efa.avv-augsburg.de/avv/",
-		DepartureMonitorEndpoint: "XML_DM_REQUEST",
-		StopFinderEndpoint:       "XML_STOPFINDER_REQUEST",
-		TripEndpoint:             "XML_TRIP_REQUEST",
-		EnableRealtime:           true,
-	},
-
-	"mvv": EFAProvider{
-		Name:                     "Münchner Verkehrs- und Tarifverbund",
-		BaseURL:                  "http://efa.mvv-muenchen.de/mvv/",
-		DepartureMonitorEndpoint: "XML_DM_REQUEST",
-		StopFinderEndpoint:       "XML_STOPFINDER_REQUEST",
-		TripEndpoint:             "XML_TRIP_REQUEST",
-		EnableRealtime:           true,
-	},
-}
