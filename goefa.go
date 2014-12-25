dc325f4f5e6aa6a3eded4a11089e9c90e36c7362
Michael Wendland <accounts@michiwend.com>
2014-12-25 15:59:48 +0100

    cleanup, docstring

--- goefa.go
+++ goefa.go
@@ -1,23 +1,28 @@
 /*
- *    Copyright (C) 2014 Michael Wendland
+ * Copyright (C) 2014 Michael Wendland
  *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Affero General Public License as published
- *    by the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
  *
- *    You should have received a copy of the GNU Affero General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *    Authors:
- *      Michael Wendland <michael@michiwend.com>
+ * Authors:
+ *   Michael Wendland <michael@michiwend.com>
  */
 
+/*
+package goefa implements a go (golang) client library to access data of public
+transport companies which provide an EFA interface. You can search a stop, get
+its next departures or request a trip.
+*/
 package goefa
 
 import (
